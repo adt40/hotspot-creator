038ddf0f6214b1283dc1a597e010c194f8966102
Austin Toot <adt40@case.edu>
2018-04-18 13:33:20 -0400

    Initial commit

---
+++ Hotspot Creator/Hotspot Creator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hotspot Creator
+//
+//  Created by Austin Toot on 4/18/18.
+//  Copyright © 2018 Austin Toot. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hotspot Creator/Hotspot Creator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hotspot Creator
+//
+//  Created by Austin Toot on 4/18/18.
+//  Copyright © 2018 Austin Toot. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
