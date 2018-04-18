109ece0a60739ce0c7a16b68b80309f1776b4bca
Austin Toot <adt40@case.edu>
2018-04-18 14:18:57 -0400

    added some code, gonna need a mac with admin privs...

--- Hotspot Creator/Hotspot Creator/ViewController.swift
+++ Hotspot Creator/Hotspot Creator/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import CoreFoundation
 
 class ViewController: NSViewController {
 
@@ -14,6 +15,21 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //edit preferences in com.apple.nat.plist
+        //https://www.amsys.co.uk/change-internet-sharing-subnet-on-mac-os-x/
+        //https://developer.apple.com/library/content/documentation/CoreFoundation/Conceptual/CFPreferences/CFPreferences.html#//apple_ref/doc/uid/10000129i
+        
+        let appID = "com.apple.nat" as CFString
+        let property = "something, idk" as CFString
+        let value = "something else" as CFString
+        
+        CFPreferencesSetValue(property, value, appID, kCFPreferencesCurrentUser, kCFPreferencesAnyHost)
+        
+        CFPreferencesSynchronize(appID, kCFPreferencesCurrentUser, kCFPreferencesAnyHost)
+        
+        
+        
     }
 
     override var representedObject: Any? {
